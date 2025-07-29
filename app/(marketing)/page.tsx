9c3aa763b1bf238137745a58695d965709d4c671
dovinhoyos <dovricarbhoy@gmail.com>
2025-07-28 20:36:31 -0500

    feat: add marketing section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-green-500 font-bold">
-        Hello lingo!
-      </p>
+      <p className="text-green-500 font-bold">Hello lingo!</p>
       <Button>Click me!</Button>
     </div>
   );
